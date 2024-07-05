ca9ac89309a1c4f0e3433ece11c2860490110e2c
Gaurav Kumar Mishra <145041800+GauravvKM1@users.noreply.github.com>
2024-07-05 22:44:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Components/Card";
+
+
+function App(){
+  // console.log("Hello! World");
+  // const data="gaurav";
+  // const name="Mishra";
+  // const cal=(12*14)/18;
+//  const data=["harsh","Gaurav","Prachi","Mishra"];
+
+  return (
+   //   {data.map((elem,index)=>(
+  //     <div key={index} className="px-3 py-4 bg-zinc-300 rounded-md w-fit">
+  //     {elem}
+  //   </div>
+  //   ))}
+   <div>
+      <Card/>
+    </div>
+
+  );
+}
+
+export default App;
